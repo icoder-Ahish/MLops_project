7cc5334c5ffab8cde4d872d1b636ed566b8c4e8c
icoder-Ahish <92961202+icoder-Ahish@users.noreply.github.com>
2026-08-17 16:37:49 +0530

    Update the resource group and Workspace name in cli/setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,6 +1,6 @@
-GROUP="RG"
-LOCATION="eastus"
-WORKSPACE="AzureMLWorkspace"
+GROUP="ashishsahu8135-rg"
+LOCATION="southindia"
+WORKSPACE="Mlopsfirst"
 
 az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
 
